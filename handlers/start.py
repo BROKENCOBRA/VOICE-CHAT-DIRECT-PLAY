c738900316b61b7783cb1d6cb8e55bd1e1fec119
anonymous <89799912+PRATHEEsni@users.noreply.github.com>
2021-09-20 13:47:58 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -3,39 +3,39 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from config import BOT_USERNAME
 
 
-@Client.on_message(filters.command(["start", "start@MemoriesMusicBot"]) & filters.private & ~filters.channel)
+@Client.on_message(filters.command(["start", "start@Ethix_Musical_Bot"]) & filters.private & ~filters.channel)
 async def start(_, message: Message):
     await message.reply_text(
         text="**Hello 👋🏻 {}!**\n\nI **Can Play Music In Voice Chats of Telegram Groups.**I Have A **lot of cool feature that will amaze You!**\n\n**Click /cmdlist For More Help On My Usage ❤**".format(message.from_user.mention),
         reply_markup=InlineKeyboardMarkup(
             [[
-            InlineKeyboardButton("➕ 𝙰𝙳𝙳 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 ➕", url="https://t.me/MemoriesMusicBot?startgroup=true")
+            InlineKeyboardButton("➕ 𝙰𝙳𝙳 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 ➕", url="https://t.me/Ethix_Musical_Bot?startgroup=true")
             ],[
-            InlineKeyboardButton("💬 𝙶𝚁𝙾𝚄𝙿", url="https://t.me/Team_Memories_Support"),
-            InlineKeyboardButton("📣 𝙲𝙷𝙰𝙽𝙽𝙴𝙻", url=f"https://t.me/Team_Memories") 
+            InlineKeyboardButton("💬 Repo Support", url="https://t.me/SHIZUKA_VC_SUPPORT"),
+            InlineKeyboardButton("📣 Owner Channel", url=f"https://t.me/aboutpratheek") 
             ],[
-            InlineKeyboardButton("😈 𝙳𝙴𝚅𝙴𝙻𝙾𝙿𝙴𝚁 ", url="https://t.me/Memory_XY")
+            InlineKeyboardButton("🥀 DEV ", url="https://t.me/pratheek06")
             ]]
         ),
         disable_web_page_preview=True
     )
         
-@Client.on_message(filters.command(["start", "start@MemoriesMusicBot"]) & ~filters.private & ~filters.channel)
+@Client.on_message(filters.command(["start", "start@Ethix_Musical_Bot"]) & ~filters.private & ~filters.channel)
 async def gstart(_, message: Message):
     await message.reply_text(
-        text="**𝙼𝙴𝙼𝙾𝚁𝙸𝙴𝚂 𝙼𝚄𝚂𝙸𝙲 𝙱𝙾𝚃 𝙸𝚂 𝙾𝙽𝙻𝙸𝙽𝙴 ✅**",
+        text="**Pratheek 𝙼𝚄𝚂𝙸𝙲 𝙱𝙾𝚃 𝙸𝚂 𝙾𝙽𝙻𝙸𝙽𝙴 ✅**",
         reply_markup=InlineKeyboardMarkup(
             [[
-            InlineKeyboardButton(text="🎙️ Support Group 🎙️", url="https://t.me/Team_Memories_Support")
+            InlineKeyboardButton(text="🎙️ Support Group 🎙️", url="https://t.me/SHIZUKA_VC_SUPPORT")
             ]]
         )
     )
 
 
-@Client.on_message(filters.command(["cmdlist", "start@MemoriesMusicBot"]) & filters.private & ~filters.channel)
+@Client.on_message(filters.command(["cmdlist", "start@Ethix_Musical_Bot"]) & filters.private & ~filters.channel)
 async def cmdlist(_, message: Message):
     await message.reply_text(
-        text="""**𝙼𝙴𝙼𝙾𝚁𝙸𝙴𝚂 𝙼𝚄𝚂𝙸𝙲 𝙱𝙾𝚃 : 𝙷𝙴𝙻𝙿 𝙼𝙴𝙽𝚄**
+        text="""**Ethix_Musical_Bot : 𝙷𝙴𝙻𝙿 𝙼𝙴𝙽𝚄**
 
 __× 𝙵𝙸𝚁𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿..
 × 𝙿𝚁𝙾𝙼𝙾𝚃𝙴 𝙼𝙴 𝙰𝚂 𝙰𝙳𝙼𝙸𝙽 𝙸𝙽 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝚆𝙸𝚃𝙷 𝙰𝙻𝙻 𝙿𝙴𝚁𝙼𝙸𝚂𝚂𝙸𝙾𝙽..__
@@ -62,7 +62,7 @@ __× 𝙵𝙸𝚁𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶�
  : __https:/t.me/Team_Memories__""",
         reply_markup=InlineKeyboardMarkup(
               [[
-              InlineKeyboardButton(text="🎙️ Support Group 🎙️", url="https://t.me/Team_Memories_Support")
+              InlineKeyboardButton(text="🎙️ Support Group 🎙️", url="https://t.me/SHIZUKA_VC_SUPPORT")
               ]]
           )
       )
