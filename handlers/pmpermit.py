7181afe7b7e0acc5ea5d524708aac2425a46fb1d
Pratheek <89387738+PratheekXD@users.noreply.github.com>
2021-09-26 00:01:29 +0530

    Update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -5,5 +5,6 @@ from pyrogram.types import Chat, Message, User
 
 @USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"Hi there, This is a music assistant service .\n\n ❗️ Rules:\n   - No chatting allowed\n   - No spam allowed \n\n 👉 **SEND GROUP INVITE LINK OR USERNAME IF USERBOT CAN'T JOIN YOUR GROUP.**\n\n ⚠️ Disclamer: If you are sending a message here it means admin will see your message and join chat\n    - Don't add this user to secret groups.\n   - Don't Share private info here\n\n")
-  return                        
+  await USER.send_message(message.chat.id,"𝑯𝒆𝒍𝒍𝒐 𝑻𝒉𝒆𝒓𝒆𐂃...\n 𝑰𝒂𝒎 𝑨 𝑷𝒓𝒊𝒗𝒂𝒕𝒆 𝑴𝒖𝒔𝒊𝒄 𝑩𝒐𝒕 𝑶𝒇 𝑷𝒓𝒂𝒕𝒉𝒆𝒆𝒌 ♫︎ .\n\n 𝚂𝙾𝚁𝚁𝚈 𝙸𝙵 𝙸 𝚆𝙾𝙽𝚃 𝚁𝙴𝙿𝙻𝚈 𝚈𝙾𝚄.. 𝙹𝚄𝚂𝚃 𝚂𝙴𝙽𝙳 𝙼𝙴 𝙶𝚁𝙾𝚄𝙿 𝙻𝙸𝙽𝙺 𝙷𝙴𝚁𝙴 𝙾𝚁 
+𝙹𝙾𝙸𝙽 @SHIZUKA_VC_SUPPORT 𝙰𝙽𝙳 𝙲𝙾𝙽𝚃𝙰𝙲𝚃 𝙰𝙳𝙼𝙸𝙽 𝙷𝙴𝚁𝙴 \n\n 𝒂𝒏𝒚 𝒊𝒔𝒔𝒖𝒆𝒔 : @pratheek06 𝒋𝒐𝒊𝒏 𝒉𝒆𝒓𝒆 : @ABOUTPRATHEEK \n\n") 
+  return                    
