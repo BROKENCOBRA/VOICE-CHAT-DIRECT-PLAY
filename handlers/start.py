820de4e1af1f14038aea4d2564cdeb2f889573a2
Tofik Denianto <77754555+tofikdn@users.noreply.github.com>
2021-09-26 15:02:23 +0700

    Create start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,6 +1,31 @@
+from time import time
+from datetime import datetime
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from config import BOT_USERNAME
+
+
+START_TIME = datetime.utcnow()
+START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
+TIME_DURATION_UNITS = (
+    ('week', 60 * 60 * 24 * 7),
+    ('day', 60 * 60 * 24),
+    ('hour', 60 * 60),
+    ('min', 60),
+    ('sec', 1)
+)
+
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return 'inf'
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append('{} {}{}'
+                         .format(amount, unit, "" if amount == 1 else "s"))
+    return ', '.join(parts)
+
 
 @Client.on_message(filters.command(["start", "start@Aturma_Vc_Bot"]) & filters.private & ~filters.channel)
 async def start(_, message: Message):
@@ -67,3 +92,26 @@ __× 𝗙𝗜𝗥𝗦𝗧 𝗔𝗗𝗗 𝗠𝗘 𝗧𝗢 𝗬𝗢𝗨𝗥 𝗚�
               ]]
           )
       )
+
+
+@Client.on_message(filters.command(["ping", "ping@Aturma_Vc_Bot"]) & ~filters.edited)
+async def ping_pong(_, message: Message):
+    start = time()
+    m_reply = await message.reply_text("pinging...")
+    delta_ping = time() - start
+    await m_reply.edit_text(
+        "🏓 `PONG!!`\n"
+        f"⚡️ `{delta_ping * 1000:.3f} ms`",
+    )
+
+
+@Client.on_message(filters.command(["uptime", "uptime@Aturma_Vc_Bot"]) & ~filters.edited)
+async def get_uptime(_, message: Message):
+    current_time = datetime.utcnow()
+    uptime_sec = (current_time - START_TIME).total_seconds()
+    uptime = await _human_time_duration(int(uptime_sec))
+    await message.reply_text(
+        "🤖 bot status:\n"
+        f"• **uptime:** `{uptime}`\n"
+        f"• **start time:** `{START_TIME_ISO}`",
+    )
