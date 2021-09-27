2c3d80c4593908415d8d354167f422a21aab83cb
Pratheek <89387738+PratheekXD@users.noreply.github.com>
2021-09-27 18:21:06 +0530

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -60,7 +60,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**🎵 𝚄𝙿𝙻𝙾𝙰𝙳𝙴𝙳 𝙱𝚢 @ **'
+        rep = '**🎵 Uᴘʟᴏᴀᴅᴇᴅ Bʏ 𝙥𝙍𝙖𝙩𝙝𝙚𝙚𝙠 🥀 **'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
