fed2f01f111b52c27308ba4edd0334c14849d4f1
Pratheek <89387738+PratheekXD@users.noreply.github.com>
2021-09-27 08:45:53 +0530

    Update audioplay.py

--- handlers/audioplay.py
+++ handlers/audioplay.py
@@ -8,7 +8,7 @@ from callsmusic import callsmusic, queues
 import converter
 from downloaders import youtube
 
-from config import BOT_NAME as bn, DURATION_LIMIT, UPDATES_CHANNEL, AUD_IMG, QUE_IMG, GROUP_SUPPORT
+from config import BOT_USERNAME as bn, DURATION_LIMIT, UPDATES_CHANNEL, AUD_IMG, QUE_IMG, GROUP_SUPPORT
 from helpers.filters import command, other_filters
 from helpers.decorators import errors
 from helpers.errors import DurationLimitError
